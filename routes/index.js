58f2926d6575082577358948d82bfaeda531024a
kyn27500 <275004595@qq.com>
2016-11-10 15:05:17 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 var resp;
+var gamenum = 5;
 
 router.get('/', function(req, res, next) {
 
   var id = req.query.id;
+  if (req.query.gameNum){
+    gamenum = req.query.gameNum;
+  }
+  
   resp = res;
   // 1-2拷贝文件
   if(id<=2){
-  	copy(id);
+
+    var copyPath = [
+      ["/Users/koba/Documents/mobile_client/meishu/ui/GameAllStar_new/res/res/csb","/Users/koba/Documents/Game/game5/res/csb"],
+      ["/Users/koba/Documents/meishu/CocosAnimation/GoldGame/quanmingxing_10.8/quanmingxing\\(3\\)/quanmingxing/res/res/an/","/Users/koba/Documents/Game/game5/res/an"]
+    ];
+  	copy(copyPath[id-1][0],copyPath[id-1][1]);
   }
   // 4.打印LUA日志，5.打Android包
   else if(id<=4){
@@ -29,8 +39,8 @@ router.get('/', function(req, res, next) {
   }
   // 拷贝东西
   else if (id <=8){
-  	var fileDir = "/Users/koba/Documents/Game/game5/"
-  	var phoneDir = "/sdcard/shouyou/apps/game5/"
+  	var fileDir = "/Users/koba/Documents/Game/game"+gamenum+"/"
+  	var phoneDir = "/sdcard/shouyou/apps/game"+gamenum+"/"
   	var copyFileName = ["cocos","res","src"]
 
   	var scriptPath = process.cwd()+ "/routes/copyToPhone.sh";
@@ -38,23 +48,34 @@ router.get('/', function(req, res, next) {
   	execShell(cmd);
   }
 
+  else if(id <=11){
+    var fileDir = "/Users/koba/Documents/game7_20160824/"
+    var phoneDir = "/sdcard/shouyou/apps/game7/"
+    var copyFileName = ["cocos","res","src"]
+
+    var scriptPath = process.cwd()+ "/routes/copyToPhone.sh";
+    var cmd = scriptPath + " " + fileDir+copyFileName[id-9]+" " + phoneDir+copyFileName[id-9]
+    execShell(cmd);
+  } 
+
+  else if (id == 12 ){
+    var sourcePath = "/Users/koba/Documents/molisishe_css/res/res/cube/csb"
+    var tagetPath = "/Users/koba/Documents/game7_20160824/res/cube/csb"
+    copy(sourcePath,tagetPath)
+  } 
   else{
-  	res.render('index', {text:"欢迎使用在线工具！"});
+
+  	res.render('index', {text:"欢迎使用在线工具！",gameNum:gamenum});
   }
 
 });
 
 
 // 拷贝文件
-function copy(id){
-
-	var copyPath = [
-		["/Users/koba/Documents/mobile_client/meishu/ui/GameAllStar_new/res/res/csb","/Users/koba/Documents/Game/game5/res/csb",".csb"],
-		["/Users/koba/Documents/meishu/CocosAnimation/GoldGame/quanmingxing_10.8/quanmingxing\\(3\\)/quanmingxing/res/res/an/","/Users/koba/Documents/Game/game5/res/an",".csb"]
-	];
+function copy(sourcePath,tagetPath){
 
 	var py = process.cwd()+"/routes/A-CopyCsb.py";
-	var cmd = py+' '+copyPath[id-1][0]+' '+copyPath[id-1][1]+' '+copyPath[id-1][2]
+	var cmd = py+' '+sourcePath+' '+tagetPath
 	execPy(cmd);
 }
 
@@ -96,7 +117,7 @@ function execShell(pCmd,pCallback){
 // print html
 function printToHtml(ptext){
 	console.log(ptext);
-	resp.render('index', {text:ptext});
+	resp.render('index', {text:ptext,gameNum:gamenum});
 }
 
 module.exports = router;
--- routes/svn.py
+++ routes/svn.py
@@ -1,64 +1,1 @@
-# -*- coding: utf-8 -*- 
-# svn 工具
-# author: kongyanan
-# date : 2016-11-03
-
-import os
-import sys
-import re
-import subprocess
-
-# svn路径(测试使用)
-svnPath = "/Users/koba/Documents/workspace/testsvn/src"
-# svnPath = "/Users/koba/Documents/mobile_client/SYC_client"
-
-
-
-# 执行系统命令
-def execSys(cmd):
-	
-	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	# for line in p.stdout.readlines():
-	#     print line,
-	retval = p.wait()
-	return retval,p.stdout.read()
-
-# 获取最新版本号
-def getVesionCode(pSvnPath):
-	cmd_svninfo = "svn info " + pSvnPath + " | grep Revision"
-	ret = execSys(cmd_svninfo)
-	if ret[0] == 0:
-		return ret[1][10:]
-
-#更新
-def svnupdate(pSvnPath):
-	cmd_svnupdate = "svn update " + pSvnPath + " | grep revision"
-	ret = execSys(cmd_svnupdate)
-
-	if ret[0]==0:
-		return ret[1][12:-2]
-def svncommit(pSvnPath):
-
-	svnadd(svnPath)
-
-	cmd_svnupdate = "svn commit " + pSvnPath + "/* -m 'robot commit' | grep revision"
-
-	ret = execSys(cmd_svnupdate)
-
-	if ret[0]==0:
-		return ret[1][19:-2]
-
-def svnadd(pSvnPath):
-	cmd_svnadd = "svn add " + pSvnPath + "/*"
-	ret = execSys(cmd_svnadd)
-	return ret[1]
-
-def test():
-	print("test----------------")
-	# str1 = getVesionCode(svnPath)
-	# str1 = svnadd(svnPath)
-	# str1 = svncommit(svnPath)
-	# print "当前版本号：" + str1
-
-if __name__ == '__main__':
-	test()
+# -*- coding: utf-8 -*- # svn 工具# author: kongyanan# date : 2016-11-03import osimport sysimport reimport subprocess# svn路径(测试使用)svnPath = "/Users/koba/Documents/workspace/testsvn/src"# svnPath = "/Users/koba/Documents/mobile_client/SYC_client"# 执行系统命令def execSys(cmd):		p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)	# for line in p.stdout.readlines():	#     print line,	retval = p.wait()	return retval,p.stdout.read()# 获取最新版本号def getVesionCode(pSvnPath):	cmd_svninfo = "svn info " + pSvnPath + " | grep Revision"	ret = execSys(cmd_svninfo)	if ret[0] == 0:		return ret[1][10:]#更新def svnupdate(pSvnPath):	cmd_svnupdate = "svn update " + pSvnPath + " | grep revision"	ret = execSys(cmd_svnupdate)	if ret[0]==0:		return ret[1][12:-2]def svncommit(pSvnPath):	svnadd(svnPath)	cmd_svnupdate = "svn commit " + pSvnPath + "/* -m 'robot commit' | grep revision"	ret = execSys(cmd_svnupdate)	if ret[0]==0:		return ret[1][19:-2]def svnadd(pSvnPath):	cmd_svnadd = "svn add " + pSvnPath + "/*"	ret = execSys(cmd_svnadd)	return ret[1]def test(cmd_type):	cmd_type = int(cmd_type)	if cmd_type==1:		str1 = svnupdate(svnPath)	elif cmd_type==2:		str1 = svnadd(svnPath)	elif cmd_type==3:		str1 = svncommit(svnPath)	elif cmd_type==4:		str1 = getVesionCode(svnPath)	return str1if __name__ == '__main__':	# 1.update,2.add,3.commit,4.getVesionCode	# global svnPath	cmd_type = 1	if len(sys.argv)==3:		svnPath = sys.argv[1]		cmd_type = sys.argv[2]	version = test(cmd_type)	print "当前版本号：" + version
